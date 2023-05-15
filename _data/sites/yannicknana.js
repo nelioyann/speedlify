cb7db09cff4e7652a6ca25f860d7a3284641210d
Yannick Nana <nanayannick1998@gmail.com>
2023-05-15 23:29:32 +0200

    fix: config

--- _data/sites/yannicknana.js
+++ _data/sites/yannicknana.js
@@ -1,19 +1,13 @@
 module.exports = {
 	name: "yannicknana.fr", // optional, falls back to object key
 	description: "My Personal web site",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 23, // 23 hours
-		// Use "run" if the sites don’t share assets on the same origin
-		//           and we can reset chrome with each run instead of
-		//           each site in every run (it’s faster)
-		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site"
+		runs: 1,
+		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://yannicknana.fr/",
-		"https://yannicknana.fr/now/",
+		"https://www.speedlify.dev/",
+		"https://frontendstories.com/",
+		"https://yannicknana.fr/"
 	]
 };
